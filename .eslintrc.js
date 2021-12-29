29163b66b4170d8ee94266b1148d21a982fcf8fd
zapzqc <zapzqc@qq.com>
2021-12-29 22:36:25 +0800

    feat: 修改 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
-  root: true, // 意思是到头啦，不要再向上找了
+  root: true,
   env: {
-    // 代码将会在哪些环境中运行。每个环境都附带了一组特定的预定义全局变量，如 browser 中有 window，document等，添加后可以直接在代码中使用，而不报错。
     browser: true,
+    es2021: true,
     node: true,
+    jest: true,
   },
   extends: [
-    'airbnb-typescript/base', // 使用airbnb风格
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-base',
+    'airbnb-typescript/base',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json', // 项目的 tsconfig.json 路径
+    ecmaVersion: 12,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json', // 通过 project 来获取 lib、jsxPragma和jsxFragmentName等参数值以及要被分析的文件（include）
   },
-  plugins: ['import'],
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
     'max-len': ['error', { code: 160 }], // 单行最大160个字符
   },
